d21b4b6af668819d4bcf4490a9a0dc8c0338ab5e
Ricky Kurniawan <rickykurniawanlie@gmail.com>
2017-12-02 08:07:44 +0700

    Set combined.log logger

--- distributed_ewallet/services/Logger.js
+++ distributed_ewallet/services/Logger.js
@@ -12,7 +12,7 @@ const logger = createLogger({
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'warn.log', level: 'warn' })
+    new transports.File({ filename: 'combined.log' }),
   ]
 });
 
